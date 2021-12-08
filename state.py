33a687d28f1ba136e7b8e44d4db2181b14ea5911
Sarah <hylrcs@gmail.com>
2021-12-08 02:19:32 +0200

    Heuristics Function 1

--- state.py
+++ state.py
@@ -1,5 +1,13 @@
-board_width = 7
-board_height = 6
+from indexing import *
+
+
+def calculate_score(computer, human, distance):
+    if human == 0:
+        computer_score = pow(10, computer) - distance
+        return computer_score, 0
+    elif computer == 0:
+        human_score = pow(10, human) - distance
+        return 0, human_score
 
 
 class State:
@@ -8,43 +16,34 @@ class State:
     def __init__(self, board):
         self.board = board
 
-    def right(self, index):
-        if index // board_height == board_width - 1:
-            return -1
-        return self.board[index + board_height]
+    def heuristic(self):
+        computer_score = 0
+        human_score = 0
+        # horizontally
+        for i in range(0, board_height):
+            temp, index = i
+            while index != -1:
+                index = temp
+                human = 0
+                computer = 0
+                distance = 0
+                for _ in range(0, 4):
+                    if self.board[index] == '1':
+                        computer += 1
+                    elif self.board[index] == '2':
+                        human += 1
+                    else:
+                        distance += self.calculate_distance(index)
+                    index = right(index)
+                temp = right(temp)
+                scores = calculate_score(computer, human, distance)
+                computer_score += scores[0]
+                human_score += scores[1]
 
-    def left(self, index):
-        if index < board_height:
-            return -1
-        return self.board[index - board_height]
-
-    def up(self, index):
-        if index % board_height == board_height - 1:
-            return -1
-        return self.board[index + 1]
-
-    def down(self, index):
-        if index % board_height == 0:
-            return -1
-        return self.board[index - 1]
-
-    def up_left(self, index):
-        if index < board_height or index % board_height == board_height - 1:
-            return -1
-        return self.board[index - board_height + 1]
-
-    def up_right(self, index):
-        if index % board_height == board_height - 1 or index // board_height == board_width - 1:
-            return -1
-        return self.board[index + board_height + 1]
-
-    def down_left(self, index):
-        if index % board_height == 0 or index < board_height:
-            return -1
-        return self.board[index - board_height - 1]
-
-    def down_right(self, index):
-        if index % board_height == 0 or index // board_height == board_width - 1:
-            return -1
-        return self.board[index + board_height - 1]
-
+    # def horizontal_count(self,start):
+    def calculate_distance(self, index):
+        distance = 0
+        while index != -1 and self.board[index] == '0':
+            distance += 1
+            index = down(index)
+        return distance
