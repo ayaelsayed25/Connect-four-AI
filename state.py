6bc0815d0305bdfd8c7e832ac92bef07a14543ef
radwaahmed20112000 <you@example.com>
2021-12-08 02:52:14 +0200

    Connect Four Diagonally and Functions Encapsulation

--- state.py
+++ state.py
@@ -21,44 +21,23 @@ class State:
         human_score = 0
         # horizontally
         for i in range(0, board_height):
-            temp, index = i
-            while index != -1:
-                index = temp
-                human = 0
-                computer = 0
-                distance = 0
-                for _ in range(0, 4):
-                    if self.board[index] == '1':
-                        computer += 1
-                    elif self.board[index] == '2':
-                        human += 1
-                    else:
-                        distance += self.calculate_distance(index)
-                    index = right(index)
-                temp = right(temp)
-                scores = calculate_score(computer, human, distance)
-                computer_score += scores[0]
-                human_score += scores[1]
+            scores = self.connect_four(self, i, right)
+            computer_score += scores[0]
+            human_score += scores[1]
         # vertically
         for i in range(0, board_width):
-            temp, index = i
-            while index != -1:
-                index = temp
-                human = 0
-                computer = 0
-                distance = 0
-                for _ in range(0, 4):
-                    if self.board[index] == '1':
-                        computer += 1
-                    elif self.board[index] == '2':
-                        human += 1
-                    else:
-                        distance += self.calculate_distance(index)
-                    index = up(index)
-                temp = up(temp)
-                scores = calculate_score(computer, human, distance)
-                computer_score += scores[0]
-                human_score += scores[1]
+            scores = self.connect_four(self, i, up)
+            computer_score += scores[0]
+            human_score += scores[1]
+        #  Diagonally
+        for i in range(0, board_height / 2):
+            scores = self.connect_four(self, i, up_right)
+            computer_score += scores[0]
+            human_score += scores[1]
+        for i in range(0, board_width / 2 * board_height, board_height):
+            scores = self.connect_four(self, i, up_right)
+            computer_score += scores[0]
+            human_score += scores[1]
 
     # def horizontal_count(self,start):
     def calculate_distance(self, index):
@@ -67,3 +46,21 @@ class State:
             distance += 1
             index = down(index)
         return distance
+
+    def connect_four(self, i, direction):
+        temp, index = i
+        while index != -1:
+            index = temp
+            human = 0
+            computer = 0
+            distance = 0
+            for _ in range(0, 4):
+                if self.board[index] == '1':
+                    computer += 1
+                elif self.board[index] == '2':
+                    human += 1
+                else:
+                    distance += self.calculate_distance(index)
+                index = direction(index)
+            temp = direction(temp)
+        return calculate_score(computer, human, distance)
