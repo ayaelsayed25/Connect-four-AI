722a08df9904256320cdc8075306ca9a44300ff7
radwaahmed20112000 <you@example.com>
2021-12-09 19:08:59 +0200

    Tree Expansion level by level

--- minimax.py
+++ minimax.py
@@ -1,22 +1,27 @@
 import math
 from state import *
 
-minimax_expansion = {}
+minimax_expansion = []
 
 
 def minimax_play(current_state, k, maximizing, prune=False):
-    minimax_expansion[current_state] = 0
+    global minimax_expansion
+    minimax_expansion = [dict() for _ in range(k + 1)]
     max_eval, next_state = minimax(current_state, k, maximizing, prune)
-    minimax_expansion.popitem()
-    board_expansion = ["" for _ in range(len(minimax_expansion))]
-    score_expansion = [-math.inf for _ in range(len(minimax_expansion))]
+    length = 0
+    for level in minimax_expansion:
+        length += len(level)
+    board_expansion = ["" for _ in range(length)]
+    score_expansion = [-math.inf for _ in range(length)]
+    minimax_expansion[k][current_state] = max_eval
     i = 0
-    minimax_expansion[current_state] = max_eval
-    for s, heuristic in minimax_expansion.items():
-        if s is not None:
-            board_expansion[i] = s.board
-            score_expansion[i] = heuristic
-        i += 1
+    for j in range(k, -1, -1):
+        print(minimax_expansion[j])
+        for state, heuristic in minimax_expansion[j].items():
+            if state is not None:
+                board_expansion[i] = state.board
+                score_expansion[i] = heuristic
+            i += 1
     return max_eval, next_state, board_expansion, score_expansion
 
 
@@ -26,35 +31,35 @@ def is_game_over(board):
 
 # prune, none
 def minimax(state, k, maximizing, prune=False, alpha=-math.inf, beta=math.inf):
+    if state is None:
+        minimax_expansion[k][State("")] = -math.inf
+        if maximizing:
+            return math.inf, None
+        else:
+            return -math.inf, None
     if k == 0 or is_game_over(state.board):
         heuristic = state.heuristic()
-        # print(heuristic)
-        minimax_expansion[state] = heuristic
+        minimax_expansion[k][state] = heuristic
         return heuristic, state
     if maximizing:
         max_eval = -math.inf
         max_state = None
         children = state.construct_next_states(True)
         for child in children:
-            minimax_expansion[child] = max_eval
-        for child in children:
-            if child is not None:
-                temp, _ = minimax(child, k - 1, False, prune, alpha, beta)
-                alpha = max(alpha, temp)
-                if temp > max_eval:
-                    max_eval = temp
-                    max_state = child
-                if prune and beta <= alpha:
-                    break
-        minimax_expansion[state] = max_eval
+            temp, _ = minimax(child, k - 1, False, prune, alpha, beta)
+            alpha = max(alpha, temp)
+            if temp > max_eval:
+                max_eval = temp
+                max_state = child
+            if prune and beta <= alpha:
+                break
+        minimax_expansion[k][state] = max_eval
         return max_eval, max_state
 
     else:
         min_eval = math.inf
         min_state = None
         children = state.construct_next_states(False)
-        for child in children:
-            minimax_expansion[child] = min_eval
         for child in children:
             if child is not None:
                 temp, _ = minimax(child, k - 1, True, prune, alpha, beta)
@@ -64,18 +69,20 @@ def minimax(state, k, maximizing, prune=False, alpha=-math.inf, beta=math.inf):
                     min_state = child
                 if prune and beta <= alpha:
                     break
-        minimax_expansion[state] = min_eval
+        minimax_expansion[k][state] = min_eval
         return min_eval, min_state
 
 
 # mama = "000000000000000000000000000000000000000000"
-# minimax_play(State("000011000002000000000000000000000000000000"), 2, True)
+m, x, d, s = minimax_play(State("000000000000000000000000000000000000111111"), 5, True, True)
+for c in s:
+    print(c)
 # # print(minimax(State("000011000002000000000000000000000000000000"), 2, True))
 # i = 0
 # j = 0
-# for state, heuristicc in minimax_expansion.items():
+# for state, heuristic in minimax_expansion.items():
 #     i += 1
-#     print(state.board + " " + str(heuristicc))
+#     print(state.board + " " + str(heuristic))
 #     if i == pow(7, j):
 #         j += 1
 #         i = 0
