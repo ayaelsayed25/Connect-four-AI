7f73d22184d165f32ba7cc57e8da685b53c5f5e6
farida752 <fere.2000.24@gmail.com>
2021-12-09 20:38:39 +0200

    show state graph

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def showGraph():
                              mode='markers',
                              name='bla',
                              marker=dict(symbol='circle-dot',
-                                         size=18,
+                                         size=30,
                                          color='#DB4551',  # '#DB4551',
                                          line=dict(color='rgb(50,50,50)', width=1)
                                          ),
@@ -113,7 +113,7 @@ def showGraph():
                              mode='markers',
                              name='bla',
                              marker=dict(symbol='circle-dot',
-                                         size=18,
+                                         size=30,
                                          color="#00294F",  # '#DB4551',
                                          line=dict(color='rgb(50,50,50)', width=1)
                                          ),
